8b6487cb37e7e7b2f3623d899bb929da9ef3f53e
Alejandro Mira Durango <89473354+alejandromiradurango@users.noreply.github.com>
2022-10-27 15:59:02 -0500

    Adding routes, images, and changing font

--- src/assets/index.js
+++ src/assets/index.js
@@ -11,8 +11,8 @@ export {default as shield} from './features/shield.svg';
 //*** CARPETA DE IMAGENES DE BANNERS ***/
 export {default as banner1} from './banners/banner1.jpg';
 export {default as banner2} from './banners/banner2.jpg';
-export {default as banner3} from './banners/banner3.jpg';
-export {default as banner4} from './banners/banner4.jpg';
+// export {default as banner3} from './banners/banner3.jpg';
+// export {default as banner4} from './banners/banner4.jpg';
 
 //*** CARPETA DEL LOGO ***/
 export {default as logo} from './logo/logo.png'
